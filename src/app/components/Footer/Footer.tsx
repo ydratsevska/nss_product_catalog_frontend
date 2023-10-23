2c2b79d0db04ff554a542182dbea49a6eae6ae21
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-23 21:17:25 +0300

    add more styles

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import './Footer.scss'
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <Link href="/" className="footer--logo">
+          <Image
+            src="/Logo.png"
+            alt="Logo"
+            width={89}
+            height={32}
+            />
+        </Link>
+      </div>
+
+      <div className="footer--links">
+        <a
+          href="https://github.com/Koliras/nss_product_catalog_frontend"
+          className="footer--links--element"
+        >
+          Github
+        </a>
+
+        <a
+          href=""
+          className="footer--links--element"
+        >
+          Contacts
+        </a>
+
+        <a
+          href=""
+          className="footer--links--element"
+        >
+          Rights
+        </a>
+      </div>
+
+      <div className="footer--back">
+        <p className="footer--back--text">
+          Back to top
+        </p>
+      </div>
+    </footer>
+  );
+}
