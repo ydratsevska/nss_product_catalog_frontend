5d38991110758dfcff8d4230fa33b076047406c5
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-26 20:47:13 +0300

    fix import

--- src/app/phones/page.tsx
+++ src/app/phones/page.tsx
@@ -1,13 +1,13 @@
-import grid from 'src/styles/modules/grid.module.scss'
-import titles from 'src/styles/modules/titles.module.scss';
-import paragraphs from 'src/styles/modules/paragraphs.module.scss';
+import grid from "src/styles/modules/grid.module.scss";
+import titles from "src/styles/modules/titles.module.scss";
+import paragraphs from "src/styles/modules/paragraphs.module.scss";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
-import phones from './page.module.scss'
+import phones from "../../styles/modules/page.module.scss";
 
 export default function Home() {
   return (
     <div className={grid.template}>
-      <BreadCrumbs category={'phones'}/>
+      <BreadCrumbs category={"phones"} />
       <h1 className={titles.main}>Mobile phones</h1>
       <p className={phones.title_sub}>95 Models</p>
 
@@ -27,5 +27,5 @@ export default function Home() {
         </select>
       </div>
     </div>
-  )
+  );
 }
