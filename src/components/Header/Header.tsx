13704699793355b1d2cac850fd336a67704b5734
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-27 16:29:08 +0300

    commit with fixed prettier

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
-"use client";
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { Navbar } from "./Navbar";
-import Logo from "./../../../public/Logo.png";
-import FavoriteIcon from "./../../../public/icons/Favourites.svg";
-import ShoppingBagIcon from "./../../../public/icons/Shopping-bag.svg";
-import closeIcon from "./../../../public/icons/Close.svg";
-import burgerMenuIcon from "./../../../public/icons/Menu.svg";
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Navbar } from './Navbar';
+import Logo from './../../../public/Logo.png';
+import FavoriteIcon from './../../../public/icons/Favourites.svg';
+import ShoppingBagIcon from './../../../public/icons/Shopping-bag.svg';
+import closeIcon from './../../../public/icons/Close.svg';
+import burgerMenuIcon from './../../../public/icons/Menu.svg';
 
-import "./Header.scss";
-import "./Navbar.scss";
-import classNames from "classnames";
-import { usePathname } from "next/navigation";
+import './Header.scss';
+import './Navbar.scss';
+import classNames from 'classnames';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
@@ -31,8 +31,8 @@ export default function Header() {
         <div className='header__icons'>
           <Link
             href='/favourites'
-            className={classNames("header__icon", "header__icon--favorite", {
-              "header__icon--active": pathName === "/favourites",
+            className={classNames('header__icon', 'header__icon--favorite', {
+              'header__icon--active': pathName === '/favourites',
             })}
           >
             <Image
@@ -45,8 +45,8 @@ export default function Header() {
 
           <Link
             href='/cart'
-            className={classNames("header__icon", "header__icon--basket", {
-              "header__icon--active": pathName === "/cart",
+            className={classNames('header__icon', 'header__icon--basket', {
+              'header__icon--active': pathName === '/cart',
             })}
           >
             <Image
@@ -78,7 +78,7 @@ export default function Header() {
       {isActive && (
         <div
           className={
-            isActive ? "header__burger-menu--active" : "header__burger-menu"
+            isActive ? 'header__burger-menu--active' : 'header__burger-menu'
           }
         >
           {<Navbar />}
@@ -86,8 +86,9 @@ export default function Header() {
           <div className='header__burger-menu-icons-bottom'>
             <Link
               href='/favourites'
-              className={classNames("header__burger-menu-icon-bottom", {
-                "header__burger-menu-icon-bottom--active": pathName === "/favourites",
+              className={classNames('header__burger-menu-icon-bottom', {
+                'header__burger-menu-icon-bottom--active':
+                  pathName === '/favourites',
               })}
             >
               <Image
@@ -99,8 +100,8 @@ export default function Header() {
             </Link>
             <Link
               href='/cart'
-              className={classNames("header__burger-menu-icon-bottom", {
-                "header__burger-menu-icon-bottom--active": pathName === "/cart",
+              className={classNames('header__burger-menu-icon-bottom', {
+                'header__burger-menu-icon-bottom--active': pathName === '/cart',
               })}
             >
               <Image
