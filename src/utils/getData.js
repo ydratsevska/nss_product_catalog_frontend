86131bc2f2cd030f57205244da2407cce7bc12c8
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-29 17:42:54 +0200

    update styles, add types, add function to fetch data

---
+++ src/utils/getData.js
@@ -0,0 +1,13 @@
+export default async function getData(type) {
+  const res = await fetch(
+    `https://nss-product-catalog-api.onrender.com/products?${
+      type ? `type=${type}` : ''
+    }`,
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
