bdb6a607fe4c1442f6b501a7b1708ae4c9162367
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-29 17:54:43 +0200

    add fetching data to tablets and accessories pages

--- src/app/tablets/page.tsx
+++ src/app/tablets/page.tsx
@@ -1,15 +1,19 @@
-import grid from 'src/styles/modules/grid.module.scss'
+import grid from 'src/styles/modules/grid.module.scss';
 import titles from 'src/styles/modules/titles.module.scss';
 import paragraphs from 'src/styles/modules/paragraphs.module.scss';
-import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs';
 import tablets from '../../styles/modules/page.module.scss';
+import getData from '@/utils/getData';
+import ProductList from '@/components/ProductList/ProductList';
 
-export default function Tablets() {
+export default async function Tablets() {
+  const data = await getData('tablets');
+
   return (
     <div className={grid.template}>
-      <BreadCrumbs category={"tablets"} />
+      <BreadCrumbs category={'tablets'} />
       <h1 className={titles.main}>Tablets</h1>
-      <p className={tablets.title_sub}>95 Models</p>
+      <p className={tablets.title_sub}>{data.count} Models</p>
 
       <div className={tablets.sort}>
         <p className={paragraphs.parameter}>Sort by</p>
@@ -26,6 +30,8 @@ export default function Tablets() {
           <option>16</option>
         </select>
       </div>
+
+      <ProductList products={data.products} />
     </div>
-  )
+  );
 }
