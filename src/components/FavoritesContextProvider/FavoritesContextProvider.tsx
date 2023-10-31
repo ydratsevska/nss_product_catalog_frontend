72b4f3789657fe42726a67a788890d2d315fa37f
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-31 11:27:01 +0200

    add handleFavorites to context

--- src/components/FavoritesContextProvider/FavoritesContextProvider.tsx
+++ src/components/FavoritesContextProvider/FavoritesContextProvider.tsx
@@ -9,12 +9,14 @@ import {
 
 type FavContext = {
   favorites: number[];
-  setFavourites: Dispatch<SetStateAction<number[]>>;
+  setFavorites: Dispatch<SetStateAction<number[]>>;
+  handleFavorites: (id: number) => void;
 };
 
 export const FavoritesContext = createContext<FavContext>({
   favorites: [],
-  setFavourites: () => undefined,
+  setFavorites: () => undefined,
+  handleFavorites: () => undefined,
 });
 
 export function FavouritesContextProvider({ children }: { children: any }) {
@@ -27,11 +29,21 @@ export function FavouritesContextProvider({ children }: { children: any }) {
     setFavorites(JSON.parse(localStorage.getItem('favorites') || '[]'));
   }, []);
 
+  const handleFavorites = (id: number) => {
+    const newFavorites = favorites.includes(id)
+      ? favorites.filter((fav) => fav !== id)
+      : [...favorites, id];
+
+    setFavorites(newFavorites);
+    localStorage.setItem('favorites', JSON.stringify(newFavorites));
+  };
+
   return (
     <FavoritesContext.Provider
       value={{
-        favorites: favorites,
-        setFavourites: setFavorites,
+        favorites,
+        setFavorites,
+        handleFavorites
       }}
     >
       {children}
